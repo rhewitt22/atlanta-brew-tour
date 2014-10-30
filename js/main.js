91b77795d779392841e4d04b7a109e10b03933f0
Roy Hewitt <royhewitt@gmail.com>
2014-10-30 19:17:34 -0400

    Created map and listing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+(function() {
+  var map, 
+    breweries,
+    markers,
+    templateScript = $("#template").html(),
+    template = Handlebars.compile(templateScript);  
+     $listing = $("#listing");
 
+console.log(templateScript);
+  function getData() {
+    $.getJSON("breweries.geojson", function (data) {
+      breweries = data;
+      markers = L.geoJson(breweries);
+      console.log(breweries);
+
+      createMap();
+      $listing.append(template(breweries));
+    });
+  }
+
+  function createMap() {
+    map = L.map('map');
+    map.fitBounds(markers.getBounds());
+    addLayers();
+  }
+
+  function addLayers() {
+    var toner = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      subdomains: 'abcd',
+      minZoom: 0,
+      maxZoom: 20
+    }).addTo(map);
+
+    map.addLayer(markers);
+  }
+
+  getData();
+})();
