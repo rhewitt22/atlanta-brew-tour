5805d562d21990496d463ecc0f0f39e8073de4d5
Roy Hewitt <royhewitt07@gmail.com>
2014-10-31 14:22:21 -0400

    Sort by open breweries

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@
   var map, 
     breweries,
     markers,
-    templateScript = $("#template").html(),
-    template = Handlebars.compile(templateScript);  
-     $listing = $("#listing");
+    source = $("#template").html(),
+    template = Handlebars.compile(source),
+    $listing = $("#listing"),
+    now = new Date(),
+    weekday = ["sunday", "monday", "tuesday", "wednesday",  "thursday", "friday", "saturday"],
+    today = weekday[now.getDay()];
 
   function getData() {
     $.getJSON("breweries.geojson", function (data) {
       breweries = data;
+      sortBreweries(breweries.features);
 
       markers = L.geoJson(breweries, {
         // pointToLayer: function (feature, latlng) {
@@ -41,9 +45,53 @@
 
     map.addLayer(markers);
   }
+  
+  function sortBreweries(breweries){
+    // Test to see if the brewery is open today
+    $.each(breweries, function(index, value){
+      var isOpen = false;
+      $.each(value.properties.schedule, function(index,value){
+        if (today === value.day) isOpen = true;
+      });
+  
+      value.properties.open = isOpen;
+    });
+  
+    // Show Open breweries at the top of the list
+    function sort(prop, arr) {
+      prop = prop.split('.');
+      var len = prop.length;
+
+      arr.sort(function (a, b) {
+          var i = 0;
+          while( i < len ) {
+              a = a[prop[i]];
+              b = b[prop[i]];
+              i++;
+          }
+          if (a < b) {
+              return -1;
+          } else if (a > b) {
+              return 1;
+          } else {
+              return 0;
+          }
+      });
+      return arr;
+    }
+
+    sort('properties.open', breweries).reverse();
+  }
 
   Handlebars.registerHelper('capitalize', function(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  Handlebars.registerHelper('twelve-hour', function(word) {
+    words = word.split(":");
+    words[0] = (parseInt(words[0]) > 12) ? words[0] - 12 : words[0];
+    return words.join(":");
+    // return word.charAt(0).toUpperCase() + word.slice(1);
   });
 
   getData();
